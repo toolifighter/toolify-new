ec5f4a567a14bcb305d67b34d1341330f4c6912b
Toolifighter <toolify.info@gmail.com>
2018-11-29 22:34:26 +0100

    automatisches stoppen

--- sources/screens/screens-measure/measure2.js
+++ sources/screens/screens-measure/measure2.js
@@ -1,10 +1,5 @@
-/* Es kann auch sein, dass dieses Programm nur auf meinem Handy genau funktioniert, weil der, 
-wenn der alle 1 ms messen soll alle 5 ms misst, wenn der alle 5 ms messen soll alle 10 ms misst und
-wenn der alle 15 ms messen soll alle 15 ms misst.
-Jetzt misst der alle 1 ms und wertet das 5 mal aus. */
-
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View, Text, Button, TextInput, ScrollView } from 'react-native';
+import { Platform, StatusBar, StyleSheet, View, Text, Button, ActivityIndicator, TextInput, ScrollView } from 'react-native';
 import { Accelerometer } from "expo";
 import styles from '../../styles';
 import {Header, Left, Right, Body, Title, Container, Icon} from 'native-base';
@@ -12,166 +7,200 @@ import {Header, Left, Right, Body, Title, Container, Icon} from 'native-base';
 export default class Bewegungsmesser extends React.Component {
    state = {
       buttontitle: "Start",
-      ausgabe: 0,
-      ausgabe2: 0,
-      fehlercode1: '',
-      fehlerindicator: false,
-      teilfehler: false,
-      durchschnittleranzeige: '',
+      ausgabeergebnis: 0,
+      ausgabedurchschnitt: 0,
+      ausgabedurchschnittcount: "",
+      fehler: false,
+      ergebnisindicator: false,
+      loading: false,
    };
 
-   helper = {
-      c: 0,
-      x: 0,
-      y: 0,
-      averageIndicator: 0,
-      teilfehler: "false",
-   };
-
+   fehlertext = ""
    measurementData = []
-   durchschnittler = []
+   calculationdurchschnitt = []
    ergebnisdurchschnitt = []
+
+   timer = 0
+   calculationtime = 0
 
    constructor(props) {
       super(props);
       Accelerometer.setUpdateInterval(1);
    }
 
-   Start = () => {
-      if (this.state.buttontitle == "Start") {
-         this.setState({
-            buttontitle: "Stop",
-            fehlercode1: '',
-            fehlerindicator: false,
-         })
-         this.durchschnittler = [],
-         this.helper.teilfehler = false,
-         Accelerometer.setUpdateInterval(1);
-         Accelerometer.addListener(accelerometerData => {
-         this.measurementData.push({
-            x: accelerometerData.x * 9.81,
-            y: accelerometerData.y * 9.81,
-            })
-         });
-         this.measurementData = []
-      } else if (this.state.buttontitle == "Stop") {
-         Accelerometer.removeAllListeners()
-         let dergebnis = 0;
-         let ergebnis = 0;
-         let sumergebnisdurchschnnitt = 0;
-         let data = this.measurementData;
-         let data1 = [];
-         let data2 = [];
-         for (t = 0; t < data.length; t++) {
-            switch (t % 2) {
-               case (0): {
-                  data1.push(data[t]);
-               }
-               break;
-               case (1): {
-                  data2.push(data[t]);
-               }
-               break;
-            }
-         }
-         this.calculation(data1);
-         this.calculation(data2);
-         if (this.helper.teilfehler == true) {
-            this.setState({
-               fehlercode1: 'Fehlermessung',
-               fehlerindicator: true,
-               buttontitle: "Start",
-            })
-         } else if (this.helper.teilfehler == false) {
-            for (let g = 0; g < this.durchschnittler.length; g++) {
-               dergebnis += this.durchschnittler[g];
-            }
-            ergebnis = dergebnis / this.durchschnittler.length;
-            this.ergebnisdurchschnitt.push(ergebnis);
-            for (let g = 0; g < this.ergebnisdurchschnitt.length; g++) {
-               sumergebnisdurchschnnitt += this.ergebnisdurchschnitt[g];
-            }
-            dsumergebnis = sumergebnisdurchschnnitt / this.ergebnisdurchschnitt.length;
-            this.setState({
-               ausgabe: Math.round(ergebnis * 100) / 100,
-               ausgabe2: Math.round(dsumergebnis * 100) / 100,
-               buttontitle: "Start",
-               durchschnittleranzeige: '(' + this.ergebnisdurchschnitt.length + ')',
-            })
-         }
-         this.measurementData = [];
-      }
-   }
-
    Loschen = () => {
+      console.log("Reset")
       if (this.state.buttontitle == "Start") {
          this.setState({
             buttontitle: "Start",
-            buttontitle2: "",
-            ausgabe: 0,
-            ausgabe2: 0,
-            durchschnittleranzeige: '',
-            fehlercode1: '',
-            fehlerindicator: false,
+            ausgabeergebnis: 0,
+            ausgabedurchschnitt: 0,
+            ausgabedurchschnittcount: "",
+            fehler: false,
+            ergebnisindicator: false,
+            loading: false,
          })
-         this.helper.c = 0;
-         this.helper.x = 0,
-         this.helper.y = 0,
-         this.helper.teilfehler = false,
-         this.helper.averageIndicator = 0;
+         this.fehlertext = ""
          this.measurementData = []
-         this.durchschnittler = []
+         this.calculationdurchschnitt = []
          this.ergebnisdurchschnitt = []
+         this.timer = 0
+         this.calculationtime = 0
       }
+   }
+
+   toggle = () => {
+      if (this.state.buttontitle == "Start") {
+         this.Start();
+      } else if (this.state.buttontitle == "Stop") {
+         this.Stop();
+      }
+   }
+   
+   Start = () => {
+      console.log("Start")
+      this.setState({
+         buttontitle: "Stop",
+         fehler: false,
+         ergebnisindicator: false,
+         loading: true,
+      })
+      let zahler = 0;
+      let schleifefalse = 0;
+      let noschleife = 0;
+      let tolleranz = 11;
+      let resetcount = tolleranz + 1;
+      this.durchschnittler = [];
+      let timer = Date.now();
+      Accelerometer.setUpdateInterval(1);
+      Accelerometer.addListener(accelerometerData => {
+         this.measurementData.push({
+            x: accelerometerData.x * 9.81,
+            y: accelerometerData.y * 9.81,
+         })
+
+         if (this.measurementData[zahler].x > -1 && this.measurementData[zahler].x < 1) {
+            this.measurementData[zahler].x = 0
+         }
+         if (this.measurementData[zahler].y > -1 && this.measurementData[zahler].y < 1) {
+            this.measurementData[zahler].y = 0
+         }
+         if (this.measurementData[zahler].x != 0 || this.measurementData[zahler].y != 0) {
+            schleifefalse ++;
+            noschleife = 0;
+            resetcount = 0;
+            console.log("Messen")
+            if (timer == null) {
+               timer = Date.now();
+            }
+         } else if (this.measurementData[zahler].x == 0 && this.measurementData[zahler].y == 0) {
+            if (schleifefalse > tolleranz) {
+               if (noschleife > tolleranz) {
+                  timer = Date.now() - timer;
+                  console.log("time: " + timer)
+                  
+                  this.measurementData.length = this.measurementData.length - 13;
+                  Accelerometer.removeAllListeners()
+                  this.calculationtime = ((timer / this.measurementData.length) / 1000);
+                  console.log("Calctime: " + this.calculationtime);
+                  console.log("Stopende");
+                  this.Stop();
+               } else {
+                  noschleife ++;
+                  console.log("Stop " + noschleife)
+               }
+            } else {
+               if (resetcount > tolleranz) {
+                  this.measurementData = [];
+                  zahler = -1;
+                  console.log("Reset: " + resetcount);
+                  timer = null;
+               } else {
+                  this.measurementData.shift();
+                  zahler --;
+                  resetcount ++;
+               }
+            }
+         }   
+         zahler ++;
+      });
+     this.measurementData = []
+   } 
+
+   Stop = () => {
+      Accelerometer.removeAllListeners()
+      this.setState({
+         buttontitle: "Start"
+      })
+      let data1 = [];
+      let data2 = [];
+      let durchschnittsumme = 0;
+      let ergebnis = 0;
+      let ergebnisdurchschnittsumme = 0;
+      let durchschnitt = 0;
+      for (let g = 0; g < this.measurementData.length; g++) {
+         if (g % 2 == 0) {
+            data1.push(this.measurementData[g]);
+         } else {
+            data2.push(this.measurementData[g]);
+         }
+      }
+      console.log(data1);
+      this.calculation(data1);
+      this.calculation(data2);
+      console.log(this.calculationdurchschnitt);
+      if (this.state.fehler == false) {
+         for (let g = 0; g < this.calculationdurchschnitt.length; g++) {
+            durchschnittsumme += this.calculationdurchschnitt[g];
+         }
+         ergebnis = durchschnittsumme / this.calculationdurchschnitt.length;
+         this.calculationdurchschnitt = [];
+         console.log(ergebnis);
+         this.ergebnisdurchschnitt.push(ergebnis);
+         for (let g = 0; g < this.ergebnisdurchschnitt.length; g++) {
+            ergebnisdurchschnittsumme += this.ergebnisdurchschnitt[g];
+         }
+         durchschnitt = ergebnisdurchschnittsumme / this.ergebnisdurchschnitt.length;
+         console.log(this.ergebnisdurchschnitt);
+         this.setState({
+            ergebnisindicator: true,
+            ausgabeergebnis: Math.round(ergebnis * 100) / 100,
+            ausgabedurchschnitt: Math.round(durchschnitt * 100) / 100,
+            ausgabedurchschnittcount: "(" + this.ergebnisdurchschnitt.length + ")",
+         })
+         console.log("geupdated: 1")
+      }
+      console.log("ende")
+      this.measurementData = [];
+      this.setState({loading: false})
    }
 
    calculation = (data) => {
       let sx = 0;
-      let vox = 0;
+      let vx = 0;
       let sy = 0;
-      let voy = 0;
-      let g = 0;
+      let vy = 0;
       let c = 0;
-      let cleanedData = data.map((a) => {
-         let x = a.x
-         if (x > -0.4 && x < 0.4) {
-            x = 0
-         }
-         let y = a.y
-         if (y > -0.4 && y < 0.4) {
-            y = 0
-         }
-         return { x: x, y: y }
-      })
-      while (this.allZero(cleanedData.slice(g, g + 10)) && g <= cleanedData.length) {
-         g++;
-      }
-      while (!this.allZero(cleanedData.slice(g, g + 10)) && g <= cleanedData.length) {
-         sx += (cleanedData[g].x / 2) * (0.01 * 0.01) + (vox * 0.01);
-         vox += (cleanedData[g].x * 0.01);
-         sy += (cleanedData[g].y / 2) * (0.01 * 0.01) + (voy * 0.01);
-         voy += (cleanedData[g].y * 0.01);
-         g++
+
+      for (let g = 0; g < data.length; g++) {
+         vx += data[g].x * this.calculationtime;
+         sx += vx * this.calculationtime;
+
+         vy += data[g].y * this.calculationtime;
+         sy += vy * this.calculationtime;
+         /*   sy += (data[g].x / 2) * (this.calculationtime * this.calculationtime) + (vx * this.calculationtime);
+         vy += (data[g].x * this.calculationtime);
+         diese Formel geht auch   */
       }
       c = Math.sqrt(sx * sx + sy * sy);
-      if (Math.abs(c) < 0.01) {
-         this.helper.teilfehler = true;
+      console.log("c: "+c);
+      if (c < 0.01) {
+         this.setState({fehler: true});
+         this.fehlertext = "zu kleine Messung";
+         console.log("zu kleine Messung")
       } else {
-         this.durchschnittler.push(c);
+         this.calculationdurchschnitt.push(c);
       }
-   }
-
-   allZero = (data) => {
-      let count = 0;
-      for (let h = 0; h < data.length; h++) {
-         if (data[h].x == 0 && data[h].y == 0) {
-            count++;
-         }
-      }
-      if (count >= data.length * 0.8) {
-         return true
-      }
-      return false
    }
 
    render() {
@@ -187,21 +216,24 @@ export default class Bewegungsmesser extends React.Component {
                   </Body>
                 </Header>
             <View style={[styles.inputContainer, {maxHeight: 350}]}>
-                <Text style={styles.inputText}>Legen Sie Ihr Smartphone auf eine gerade Unterlage. Starten Sie die Messung und bewgen Sie das Gerät zügig zur Zielposition. Stoppen Sie die Messung. Führen sie diese Messung erneut in entgegengesetzte Richtung durch, um Ungenauigkeiten zu vermeiden. Sie können auch weitere Messungen durchführen.</Text>
+               <Text style={styles.inputText}>Legen Sie Ihr Smartphone auf eine gerade Unterlage. Starten Sie die Messung und bewgen Sie das Gerät zügig zur Zielposition. Stoppen Sie die Messung. Führen sie diese Messung erneut in entgegengesetzte Richtung durch, um Ungenauigkeiten zu vermeiden. Sie können auch weitere Messungen durchführen.</Text>
             </View>
-                <View style={styles.outputContainer}>
-                    <Button onPress={this.Start} title={this.state.buttontitle}></Button>
-                </View>           
-                <View style={styles.outputContainer}>
-                    {this.state.indicatorStatus ? <ActivityIndicator size="large"/> : null}
-                    <View style={[styles.notification, styles.outputContainer, {backgroundColor: '#ccc'}]}>
-                        <Text style={styles.outputText}>Aktuelle Messung: {this.state.ausgabe} m</Text>
-                        <Text style={styles.outputText}>Durchschnitt: {this.state.ausgabe2} m</Text>
-                    </View>
-                    <View style={styles.outputContainer}>
-                        <Button onPress={this.Loschen} title="Reset"/>
-                    </View>
-                </View>
+               <View style={styles.outputContainer}>
+                  <Button onPress={this.toggle} title={this.state.buttontitle}></Button>
+               </View>           
+               <View style={styles.outputContainer}>
+                  {this.state.loading ? <ActivityIndicator size="large"/> : null}
+                  <View style={[styles.notification, styles.outputContainer, {backgroundColor: '#ccc'}]}>
+                     <Text style={styles.outputText}>Aktuelle Messung: {this.state.ausgabeergebnis} m</Text>
+                     <Text style={styles.outputText}>Durchschnitt{this.state.ausgabedurchschnittcount}: {this.state.ausgabedurchschnitt} m</Text>
+                  </View>
+                  {this.state.fehler ? <View style={[styles.outputContainer, {backgroundColor: '#f00'}, styles.notification]}>
+                  <Text style={styles.outputText}>{this.state.fehlertext}</Text>
+                  </View> : null}
+                  <View style={styles.outputContainer}>
+                     <Button onPress={this.Loschen} title="Reset"/>
+                  </View>
+               </View>
             </View>
         </ScrollView>
       )
